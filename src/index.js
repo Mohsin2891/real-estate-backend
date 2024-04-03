2b2162ef5679d7d9bc5fe903202b1b39f80e7077
mohsin hassan <mohsin.hassan2891@nxb.com.pk>
2024-04-03 16:23:43 +0500

    changed db

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
-
+console.log('console.log()', config.mongoose.url);
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
